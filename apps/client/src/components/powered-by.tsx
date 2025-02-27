a82426119e09ae6d0651f091c4bf0fe6866fea04
Paul George <paultibulca@gmail.com>
2025-02-27 16:10:48 +0000

    Updated styling

--- apps/client/src/components/powered-by.tsx
+++ apps/client/src/components/powered-by.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
+import { Logo } from "./logo";
 
 export function PoweredBy({ className }: { className?: string }) {
     return (
         <a href="https://supaboard.io" target="_blank" rel="noopener noreferrer" className={cn("text-xs font-extralight text-gray-400", className)}>
-            Powered by SupaBoard
+            Powered by <Logo className="inline-block size-4" /> <strong>supaboard</strong>
         </a>
     )
 }
